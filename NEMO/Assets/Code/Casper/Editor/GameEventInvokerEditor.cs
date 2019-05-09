e2f5ebc9f86764893e1b504fc8b4af4285a029c4
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-05-09 17:18:42 +0200

    End cutscene event on trigger

---
+++ NEMO/Assets/Code/Casper/Editor/GameEventInvokerEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameEventInvoker))]
+public class GameEventInvokerEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		GameEventInvoker gameEventInvoker = (GameEventInvoker)target;
+		if (GUILayout.Button("Execute"))
+		{
+			gameEventInvoker.Execute();
+		}
+	}
+}
---
+++ NEMO/Assets/Code/Casper/GameEventInvoker.cs
@@ -0,0 +1,40 @@
+﻿using CM.Essentials.Timing;
+using System.Collections;
+using UnityEngine;
+
+public class GameEventInvoker : MonoBehaviour
+{
+	public GameEvent gameEventToInvoke;
+	public bool delay;
+	public TimeData delayTime;
+
+	public void Execute()
+	{
+		if (delay)
+		{
+			StartCoroutine(Routine());
+		}
+		else
+		{
+			InvokeGameEvent();
+		}
+	}
+
+	public void Execute(TimeData delayTime)
+	{
+		delay = true;
+		this.delayTime = delayTime;
+		Execute();
+	}
+
+	private IEnumerator Routine()
+	{
+		yield return new WaitForSeconds(delayTime.TotalSeconds);
+		InvokeGameEvent();
+	}
+
+	private void InvokeGameEvent()
+	{
+		gameEventToInvoke.Invoke();
+	}
+}
