16cfcce1f6438c73bd16efa98075ed9b2e43380f
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-05-13 11:42:45 +0200

    Audio Triggers

---
+++ NEMO/Assets/Code/Casper/AudioFader.cs
@@ -0,0 +1,43 @@
+﻿using CM.Essentials.Timing;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioFader : MonoBehaviour
+{
+	public TimeData time;
+	[Range(0, 1)]
+	public float maxVolume = 1;
+
+	private AudioSource _audioSource;
+
+	private TimeInterpolationFloat _timeInterpolationFloat;
+
+	private void Awake()
+	{
+		_audioSource = GetComponent<AudioSource>();
+	}
+
+	public void FadeIn()
+	{
+		_audioSource.Play();
+		_timeInterpolationFloat = TimeInterpolationFloat.InterpolateTo(gameObject, _audioSource.volume, maxVolume, time, null);
+	}
+
+	public void FadeOut()
+	{
+		_timeInterpolationFloat = TimeInterpolationFloat.InterpolateTo(gameObject, _audioSource.volume, 0, time, StopAudio);
+	}
+
+	private void Update()
+	{
+		if (_timeInterpolationFloat)
+		{
+			_audioSource.volume = _timeInterpolationFloat.Value;
+		}
+	}
+
+	private void StopAudio()
+	{
+		_audioSource.Stop();
+	}
+}
