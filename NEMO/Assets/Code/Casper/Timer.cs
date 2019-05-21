655f320ea3a538f2cf2abe86e0f0d503af18dadc
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-05-21 20:31:14 +0200

    End cutscene backwards movement

---
+++ NEMO/Assets/Code/Casper/Timer.cs
@@ -0,0 +1,33 @@
+﻿using CM.Essentials.Timing;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+	public TimeData time;
+
+	public UnityEvent OnTimerFinished;
+
+	public void Execute()
+	{
+		StartCoroutine(Routine());
+	}
+
+	public void Execute(TimeData time)
+	{
+		this.time = time;
+		Execute();
+	}
+
+	private IEnumerator Routine()
+	{
+		yield return new WaitForSeconds(time.TotalSeconds);
+		TimerFinished();
+	}
+
+	private void TimerFinished()
+	{
+		OnTimerFinished.Invoke();
+	}
+}
