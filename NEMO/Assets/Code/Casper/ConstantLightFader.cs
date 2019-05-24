56c04490f5b7c423892252fc6f50d71474c0ba57
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-05-24 12:31:14 +0200

    Added BreathingLight prefab

---
+++ NEMO/Assets/Code/Casper/ConstantLightFader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace CM.Essentials
+{
+	[RequireComponent(typeof(Light))]
+	public class ConstantLightFader : LightFader
+	{
+		private void Start()
+		{
+			FadeIn();
+		}
+
+		protected override void OnFadeInFinish()
+		{
+			base.OnFadeInFinish();
+
+			FadeOut();
+		}
+
+		protected override void OnFadeOutFinish()
+		{
+			base.OnFadeOutFinish();
+
+			FadeIn();
+		}
+	}
+}
