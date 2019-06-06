7dc0f8617eb3c2a630ae82edc06a5c189eaf39e1
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-06-06 16:46:40 +0200

    Added a CorrectedBreathingEventManager

---
+++ NEMO/Assets/Code/Casper/CorrectedBreathingEventManager.cs
@@ -0,0 +1,62 @@
+﻿using CM.Essentials.Timing;
+using System.Collections;
+using UnityEngine;
+
+public class CorrectedBreathingEventManager : MonoBehaviour
+{
+	public TimeData breathingDetectionDelay;
+
+	[Header("Corrected Breathing Events")]
+	public GameEvent CorrectedBreathingInEvent;
+	public GameEvent CorrectedBreathingOutEvent;
+
+	private bool _isBreathingIn = false;
+
+	public void BreathingIn()
+	{
+		_isBreathingIn = true;
+		StartCoroutine(Routine(breathingDetectionDelay, _isBreathingIn));
+	}
+
+	public void BreathingOut()
+	{
+		_isBreathingIn = false;
+		StartCoroutine(Routine(breathingDetectionDelay, _isBreathingIn));
+	}
+
+	private IEnumerator Routine(TimeData time, bool isBreathingIn)
+	{
+		bool valueChanged = false;
+
+		while (time.TotalSeconds > 0)
+		{
+			if (_isBreathingIn != isBreathingIn)
+			{
+				valueChanged = true;
+				yield break;
+			}
+
+			time -= Time.deltaTime;
+			yield return null;
+		}
+
+		if (!valueChanged)
+		{
+			CorrectedBreathing(isBreathingIn);
+		}
+	}
+
+	private void CorrectedBreathing(bool isBreathingIn)
+	{
+		if (isBreathingIn)
+		{
+			CorrectedBreathingInEvent.Invoke();
+			Debug.Log("BREATHING IN");
+		}
+		else
+		{
+			CorrectedBreathingOutEvent.Invoke();
+			Debug.Log("BREATHING OUT");
+		}
+	}
+}
