fc2afb1482f3109cf6ecd3013fab34fde7582842
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-06-07 14:11:23 +0200

    Added MutateWithBreathFader

---
+++ NEMO/Assets/Code/Casper/MutateWithBreathFader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace CM.Essentials
+{
+	public class MutateWithBreathFader : FloatFader<MutateWithBreathFader>
+	{
+		public string nodeName = "globalBreathValue";
+
+		protected override float GetComponentValue()
+		{
+			return Shader.GetGlobalFloat(nodeName);
+		}
+
+		protected override void SetComponentValue(float value)
+		{
+			Shader.SetGlobalFloat(nodeName, value);
+		}
+	}
+}
