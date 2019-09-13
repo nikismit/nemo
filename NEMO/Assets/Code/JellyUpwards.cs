446d12960dac80da948c065794c0c60e5142d3d9
niels <niels-duivenvoorden@hotmail.com>
2019-09-13 12:04:47 +0200

    portal card niels

--- NEMO/Assets/Code/JellyUpwards.cs
+++ NEMO/Assets/Code/JellyUpwards.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class JellyUpwards : MonoBehaviour
 {
     [Header("Jelly Selection")]
-    //public GameObject[] JellyClumps;
     public Transform[] Jellies;
 
     [Header("Values")]
-    public float minRandomMovUp = .1f, maxRandomMovUp = .5f, minShiver = -.5f, maxShiver = .5f;
+    public float minRandomMovUp = .1f, maxRandomMovUp = .2f, minShiver = -.1f, maxShiver = .1f, rotationSpeed = .5f;
 
     public bool AutoUp = true;
 
@@ -17,6 +16,7 @@ public class JellyUpwards : MonoBehaviour
 
     private float[] upFactor;
     private float[] shiverFactor;
+    private float[] rotationFactor;
     private Vector3[] startPos;
 
     private int Amount;
@@ -29,40 +29,25 @@ public class JellyUpwards : MonoBehaviour
 
         upFactor = new float[Amount];
         shiverFactor = new float[Amount];
+        rotationFactor = new float[Amount];
         startPos = new Vector3[Amount];
 
         for (int i = 0; i < Jellies.Length; i++)
         {
             CalculateUp();
             CalculateShiver();
-            //startPos[i] = JellyClumps[i].transform.position;
+            CalculateRotation();
             startPos[i] = Jellies[i].transform.position;
         }
     }
 
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.J))
-        // {
-        //     if (!GoUp)
-        //     {
-        //         GoUp = true;
-        //     }
-        //     else if (GoUp)
-        //     {
-        //         GoUp = false;
-        //     }
-        // }
-
-        // if (Input.GetKeyDown(KeyCode.R))
-        // {
-        //     Reset();
-        // }
-
         if (GoUp || AutoUp)
         {
             JellyGoUp();
             JellyMoveHor();
+            JellyRotate();
         }
     }
 
@@ -70,7 +55,6 @@ public class JellyUpwards : MonoBehaviour
     {
         for (int i = 0; i < Jellies.Length; i++)
         {
-            //JellyClumps[i].transform.position += Vector3.up * Time.deltaTime * upFactor[i];
             Jellies[i].transform.position += Vector3.up * Time.deltaTime * upFactor[i];
         }
     }
@@ -79,22 +63,17 @@ public class JellyUpwards : MonoBehaviour
     {
         for (int i = 0; i < Jellies.Length; i++)
         {
-            //JellyClumps[i].transform.position += Vector3.right * Time.deltaTime * shiverFactor[i];
             Jellies[i].transform.position += Vector3.right * Time.deltaTime * shiverFactor[i];
         }
     }
 
-    // void Reset()
-    // {
-    //     for (int i = 0; i < Jellies.Length; i++)
-    //     {
-    //         //JellyClumps[i].transform.position = startPos[i];
-    //         Jellies[i].transform.position = startPos[i];
-    //     }
-    //     CalculateUp();
-    //     CalculateShiver();
-    //     print("reset");
-    // }
+    void JellyRotate()
+    {
+        for (int i = 0; i < Jellies.Length; i++)
+        {
+            Jellies[i].transform.Rotate(new Vector3(0, rotationFactor[i] * rotationSpeed, 0));
+        }
+    }
 
     void CalculateUp()
     {
@@ -102,7 +81,6 @@ public class JellyUpwards : MonoBehaviour
         {
             upFactor[i] = Random.Range(minRandomMovUp, maxRandomMovUp);
         }
-        //print("calculated upwards momentum");
     }
 
     void CalculateShiver()
@@ -111,6 +89,13 @@ public class JellyUpwards : MonoBehaviour
         {
             shiverFactor[i] = Random.Range(minShiver, maxShiver);
         }
-        //print("calculated shiver momentum");
+    }
+
+    void CalculateRotation()
+    {
+        for (int i = 0; i < Jellies.Length; i++)
+        {
+            rotationFactor[i] = Random.Range(-1f, 1f);
+        }
     }
 }
---
+++ NEMO/Assets/niels/scripts/fadeWhenCloser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeWhenCloser : MonoBehaviour
+{
+    public GameObject objectToWatch;
+    public float triggerDistance = 10f;
+    public Color startColor = Color.black, newColor = new Color(1f, 1f, 1f, 0f);
+
+    private float distance;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (objectToWatch == null)
+        {
+            print("forgot to put player in inspector");
+        }
+
+        sr = GetComponent<SpriteRenderer>();
+        sr.color = startColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, objectToWatch.transform.position);
+
+        if (distance < triggerDistance)
+        {
+            sr.color = Color.Lerp(GetComponent<SpriteRenderer>().color, newColor, 0.1f);
+        }
+        else
+        {
+            sr.color = Color.Lerp(GetComponent<SpriteRenderer>().color, startColor, 0.1f);
+        }
+    }
+}
