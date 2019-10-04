fdac7578652a92074598f967953507c0f94c9824
niels <niels-duivenvoorden@hotmail.com>
2019-10-04 12:33:44 +0200

    scene 11, oud canvas hard fix
        void OnDisable()
        {
            _isRoutineRunning = false;
            _isGrowRoutine = false;
        }
    
    added, which resets the bools at all times when disabled

--- NEMO/Assets/Code/Casper/TutorialGrowCheck.cs
+++ NEMO/Assets/Code/Casper/TutorialGrowCheck.cs
@@ -6,133 +6,139 @@ using UnityEngine;
 [RequireComponent(typeof(ObjectScaler))]
 public class TutorialGrowCheck : MonoBehaviour
 {
-	[Header("Threshold in seconds")]
-	public float range = 0.5f;
-	public int counterToReach = 4;
+    [Header("Threshold in seconds")]
+    public float range = 0.5f;
+    public int counterToReach = 4;
 
-	[Header("Game Events")]
-	public GameEvent CorrectEvent;
-	public GameEvent WrongEvent;
-	public GameEvent FinishedEvent;
-	public GameEvent TutorialExhalingEvent;
-	public GameEvent TutorialInhalingEvent;
+    [Header("Game Events")]
+    public GameEvent CorrectEvent;
+    public GameEvent WrongEvent;
+    public GameEvent FinishedEvent;
+    public GameEvent TutorialExhalingEvent;
+    public GameEvent TutorialInhalingEvent;
 
-	private ObjectScaler _objectScaler;
-	private bool _isRoutineRunning = false;
-	private bool _isGrowRoutine = false;
-	private int _counter = 0;
+    private ObjectScaler _objectScaler;
+    private bool _isRoutineRunning = false;
+    private bool _isGrowRoutine = false;
+    private int _counter = 0;
 
-	private void Awake()
-	{
-		_objectScaler = GetComponent<ObjectScaler>();
+    private void Awake()
+    {
+        _objectScaler = GetComponent<ObjectScaler>();
 
-		CM_Debug.AddCategory("NEMO Tutorial");
-	}
+        CM_Debug.AddCategory("NEMO Tutorial");
+    }
 
-	public void ExecuteInhaling()
-	{
-		if (!_isGrowRoutine)
-		{
-			_counter = 0;
-			InvokeWrongEvent();
-			CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
-			return;
-		}
+    public void ExecuteInhaling()
+    {
+        if (!_isGrowRoutine)
+        {
+            _counter = 0;
+            InvokeWrongEvent();
+            CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
+            return;
+        }
 
-		if (!_isRoutineRunning)
-		{
-			_counter = 0;
-			InvokeWrongEvent();
-			CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
-			return;
-		}
+        if (!_isRoutineRunning)
+        {
+            _counter = 0;
+            InvokeWrongEvent();
+            CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
+            return;
+        }
 
-		_counter++;
-		InvokeCorrectEvent();
-		CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
+        _counter++;
+        InvokeCorrectEvent();
+        CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
 
-		InvokeFinishedEvent();
-	}
+        InvokeFinishedEvent();
+    }
 
-	public void ExecuteExhaling()
-	{
-		if (_isGrowRoutine)
-		{
-			_counter = 0;
-			InvokeWrongEvent();
-			CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
-			return;
-		}
+    public void ExecuteExhaling()
+    {
+        if (_isGrowRoutine)
+        {
+            _counter = 0;
+            InvokeWrongEvent();
+            CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
+            return;
+        }
 
-		if (!_isRoutineRunning)
-		{
-			_counter = 0;
-			InvokeWrongEvent();
-			CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
-			return;
-		}
+        if (!_isRoutineRunning)
+        {
+            _counter = 0;
+            InvokeWrongEvent();
+            CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
+            return;
+        }
 
-		_counter++;
-		InvokeCorrectEvent();
-		CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
+        _counter++;
+        InvokeCorrectEvent();
+        CM_Debug.Log("NEMO Tutorial", "COUNTER: " + _counter);
 
-		InvokeFinishedEvent();
-	}
+        InvokeFinishedEvent();
+    }
 
-	public void Reset()
-	{
-		_counter = 0;
-	}
+    public void Reset()
+    {
+        _counter = 0;
+    }
 
-	private IEnumerator Routine(bool isGrowing)
-	{
-		_isRoutineRunning = true;
-		_isGrowRoutine = isGrowing;
-		yield return new WaitForSeconds(range * 2);
-		_isRoutineRunning = false;
-	}
+    private IEnumerator Routine(bool isGrowing)
+    {
+        _isRoutineRunning = true;
+        _isGrowRoutine = isGrowing;
+        yield return new WaitForSeconds(range * 2);
+        _isRoutineRunning = false;
+    }
 
-	private void Update()
-	{
-		// Growing
-		if (_objectScaler.IsGrowing)
-		{
-			if (_objectScaler.CurrentGrowTime >= _objectScaler.inhalingSeconds - range)
-			{
-				if (!_isRoutineRunning)
-				{
-					StartCoroutine(Routine(false));
-					TutorialExhalingEvent.Invoke();
-				}
-			}
-		}
-		// Not growing
-		else
-		{
-			if (_objectScaler.CurrentGrowTime >= _objectScaler.exhalingSeconds - range)
-			{
-				if (!_isRoutineRunning)
-				{
-					StartCoroutine(Routine(true));
-					TutorialInhalingEvent.Invoke();
-				}
-			}
-		}
-	}
+    private void Update()
+    {
+        // Growing
+        if (_objectScaler.IsGrowing)
+        {
+            if (_objectScaler.CurrentGrowTime >= _objectScaler.inhalingSeconds - range)
+            {
+                if (!_isRoutineRunning)
+                {
+                    StartCoroutine(Routine(false));
+                    TutorialExhalingEvent.Invoke();
+                }
+            }
+        }
+        // Not growing
+        else
+        {
+            if (_objectScaler.CurrentGrowTime >= _objectScaler.exhalingSeconds - range)
+            {
+                if (!_isRoutineRunning)
+                {
+                    StartCoroutine(Routine(true));
+                    TutorialInhalingEvent.Invoke();
+                }
+            }
+        }
+    }
 
-	private void InvokeFinishedEvent()
-	{
-		if (_counter >= counterToReach)
-			FinishedEvent.Invoke();
-	}
+    private void InvokeFinishedEvent()
+    {
+        if (_counter >= counterToReach)
+            FinishedEvent.Invoke();
+    }
 
-	private void InvokeCorrectEvent()
-	{
-		CorrectEvent.Invoke();
-	}
+    private void InvokeCorrectEvent()
+    {
+        CorrectEvent.Invoke();
+    }
 
-	private void InvokeWrongEvent()
-	{
-		WrongEvent.Invoke();
-	}
+    private void InvokeWrongEvent()
+    {
+        WrongEvent.Invoke();
+    }
+
+    void OnDisable()
+    {
+        _isRoutineRunning = false;
+        _isGrowRoutine = false;
+    }
 }
