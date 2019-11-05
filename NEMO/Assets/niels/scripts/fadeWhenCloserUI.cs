37b88393985e24e0cd0b8b13f0118faba42fe484
nielsduif <niels-duivenvoorden@hotmail.com>
2019-11-05 17:56:32 +0100

    fix bellen on movement, textmeshes bewegen naar achter, roteren

--- NEMO/Assets/niels/scripts/fadeWhenCloserUI.cs
+++ NEMO/Assets/niels/scripts/fadeWhenCloserUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(BoxCollider))]
 public class fadeWhenCloserUI : MonoBehaviour
 {
     public GameObject objectToWatch;
@@ -14,9 +15,24 @@ public class fadeWhenCloserUI : MonoBehaviour
     private TMP_Text TMPT;
     private bool goneThru;
 
+    private BoxCollider BC;
+
+    private Vector3 startPos;
+    private Quaternion startRot;
+
+    public float startMoveDistance = 5;
+    public float moveSpeed = 5f;
+    // private bool checkBool;
+
+    public float rotateAngle;
+    public float rotationSpeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
+        startPos = transform.position;
+        startRot = transform.rotation;
+
         goneThru = false;
 
         if (objectToWatch == null)
@@ -30,6 +46,9 @@ public class fadeWhenCloserUI : MonoBehaviour
 
         TMPT = GetComponent<TextMeshPro>();
         startColor = TMPT.color;
+
+        BC = GetComponent<BoxCollider>();
+        BC.isTrigger = true;
     }
 
     // Update is called once per frame
@@ -37,19 +56,53 @@ public class fadeWhenCloserUI : MonoBehaviour
     {
         distance = Vector3.Distance(transform.position, objectToWatch.transform.position);
 
-        if (distance > triggerDistance && !goneThru || distance < closeTriggerDistance && !goneThru)
+        if (distance > triggerDistance || distance < closeTriggerDistance)
         {
+            //fade out
             TMPT.color = Color.Lerp(GetComponent<TextMeshPro>().color, newColor, fadeSpeed / 100);
         }
         else
         {
+            //fade in
             TMPT.color = Color.Lerp(GetComponent<TextMeshPro>().color, startColor, fadeSpeed / 100);
         }
 
-        if (TMPT.color.a == 0 && !goneThru)
+        if (goneThru)
+        {
+            if (distance > startMoveDistance)
+            {
+                transform.position += new Vector3(0, 0, moveSpeed / 100);
+                transform.Rotate(new Vector3(0, 0, rotateAngle) * (rotationSpeed * Time.deltaTime));
+                // checkBool = true;
+            }
+
+            // if (distance < startMoveDistance && checkBool)
+            // {
+            //     transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, objectToWatch.transform.position.z + 1), 100);
+            // }
+
+            if (distance > triggerDistance + 5)
+            {
+                goneThru = false;
+                gameObject.SetActive(false);
+                ResetTextMeshProPosition();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.name);
+        if (other.gameObject == objectToWatch)
         {
             goneThru = true;
-            gameObject.SetActive(false);
         }
+    }
+
+    public void ResetTextMeshProPosition()
+    {
+        goneThru = false;
+        transform.position = startPos;
+        transform.rotation = startRot;
     }
 }
